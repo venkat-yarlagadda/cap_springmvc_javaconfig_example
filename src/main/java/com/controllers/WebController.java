57b3453d5af901ca843cac972f4088653f26681c
Venkata Kumar <venkat.yarlagadda87@gmail.com>
2022-02-16 17:21:42 +0530

    Added JSTL

--- src/main/java/com/controllers/WebController.java
+++ src/main/java/com/controllers/WebController.java
@@ -1,5 +1,8 @@
 package com.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
@@ -18,7 +21,19 @@ public class WebController {
 	
 	@RequestMapping(value = "/user",method = RequestMethod.POST)
 	public String userPage(@Validated User user, Model model) {
-		model.addAttribute("user",user);
+		List<User> l = new ArrayList();
+		l.add(user);
+		User u = new User();
+		u.setUserName("Shakir");
+		User u1 = new User();
+		u1.setUserName("Kumar");
+		User u2 = new User();
+		u2.setUserName("Jyothi");
+		l.add(u);
+		l.add(u1);
+		l.add(u2);
+		model.addAttribute("user",l);
+		
 		return "user";
 	}
 }
--- target/SpringMVCAuto-0.0.1-SNAPSHOT/WEB-INF/views/user.jsp
+++ target/SpringMVCAuto-0.0.1-SNAPSHOT/WEB-INF/views/user.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+    <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +8,9 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h1>Hello ${user.userName}</h1>
+<h1>Hello All</h1>
+<c:forEach var="list" items="${user}">
+	<h2> ${list.userName}</h2>
+</c:forEach>
 </body>
 </html>
