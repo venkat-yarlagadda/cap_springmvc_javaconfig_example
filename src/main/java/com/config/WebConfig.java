cea334620d40db690136bbe7914a314ebd04adc4
Venkata Kumar <venkat.yarlagadda87@gmail.com>
2022-02-16 16:52:28 +0530

    Spring MVC Java config

---
+++ src/main/java/com/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages = "com")
+@EnableWebMvc
+public class WebConfig {
+	@Bean
+	public ViewResolver configureViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+}
